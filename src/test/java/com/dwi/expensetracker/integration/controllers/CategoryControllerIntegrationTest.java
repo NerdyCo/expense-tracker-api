075e9e6a48964908b687922a618746d2fab5d36d
NerdyCo <kautsarteguh2002@gmail.com>
2025-06-04 16:39:17 +0700

    refactor integration test for category controller

--- src/test/java/com/dwi/expensetracker/integration/controllers/CategoryControllerIntegrationTest.java
+++ src/test/java/com/dwi/expensetracker/integration/controllers/CategoryControllerIntegrationTest.java
@@ -4,158 +4,139 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.dwi.expensetracker.TestDataUtil;
+import com.dwi.expensetracker.domains.dtos.category.CategoryPatchDto;
 import com.dwi.expensetracker.domains.dtos.category.CategoryRequestDto;
-import com.dwi.expensetracker.domains.dtos.user.UserRequestDto;
 import com.dwi.expensetracker.domains.entities.Category;
 import com.dwi.expensetracker.domains.entities.User;
 import com.dwi.expensetracker.services.CategoryService;
+import com.dwi.expensetracker.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Transactional
+@TestExecutionListeners(value = TransactionalTestExecutionListener.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class CategoryControllerIntegrationTest {
         private final MockMvc mockMvc;
         private final CategoryService categoryService;
+        private final UserService userService;
         private final ObjectMapper objectMapper;
 
         @Autowired
-        public CategoryControllerIntegrationTest(
-                        MockMvc mockMvc,
-                        CategoryService categoryService,
-                        ObjectMapper objectMapper) {
+        public CategoryControllerIntegrationTest(MockMvc mockMvc, CategoryService categoryService,
+                        UserService userService, ObjectMapper objectMapper) {
                 this.mockMvc = mockMvc;
                 this.categoryService = categoryService;
+                this.userService = userService;
                 this.objectMapper = objectMapper;
         }
 
-        @Test
-        public void testCreateCategoryReturns201() throws Exception {
-                UserRequestDto customerDto = TestDataUtil.createTestCustomerDtoA();
-                CategoryRequestDto categoryDto = TestDataUtil.createTestCategoryDtoA(customerDto);
-                String categoryJson = objectMapper.writeValueAsString(categoryDto);
-
-                mockMvc.perform(post("/api/categories")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(categoryJson))
-                                .andExpect(status().isCreated());
-        }
+        private static final String BASE_URL = "/api/v1/categories";
 
         @Test
-        public void testCreateCategoryReturnsSavedCategory() throws Exception {
-                UserRequestDto customerDto = TestDataUtil.createTestCustomerDtoA();
-                CategoryRequestDto categoryDto = TestDataUtil.createTestCategoryDtoA(customerDto);
-                String categoryJson = objectMapper.writeValueAsString(categoryDto);
+        @DisplayName("1. Should create category and return 201 CREATED")
+        public void shouldCreateCategoryAndReturn201() throws Exception {
+                User user = userService.create(TestDataUtil.givenUserA());
+                CategoryRequestDto requestDto = TestDataUtil.givenCategoryDtoA(user.getId());
 
-                mockMvc.perform(post("/api/categories")
+                String json = objectMapper.writeValueAsString(requestDto);
+
+                mockMvc.perform(post(BASE_URL)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(categoryJson))
+                                .content(json))
                                 .andExpect(status().isCreated())
                                 .andExpect(jsonPath("$.id").exists())
-                                .andExpect(jsonPath("$.name").value("Food & Beverage"))
-                                .andExpect(jsonPath("$.customer").exists())
-                                .andExpect(jsonPath("$.customer.id").exists())
-                                .andExpect(jsonPath("$.customer.username").value(customerDto.getUsername()));
+                                .andExpect(jsonPath("$.name").value("Food & Beverage"));
         }
 
         @Test
-        public void testGetCategoryReturns200WhenExists() throws Exception {
-                User customerEntity = TestDataUtil.createTestCustomerEntityA();
-                Category categoryEntity = categoryService.save(TestDataUtil.createTestCategoryEntityA(customerEntity));
+        @DisplayName("2. Should get category by ID and return 200 OK")
+        public void shouldGetCategoryById() throws Exception {
+                User user = userService.create(TestDataUtil.givenUserA());
+                Category category = categoryService.create(TestDataUtil.givenCategoryA(user));
 
-                mockMvc.perform(get("/api/categories/" + categoryEntity.getId()))
-                                .andExpect(status().isOk());
-        }
-
-        @Test
-        public void testThatCategoryReturnsCorrectData() throws Exception {
-                User customerEntity = TestDataUtil.createTestCustomerEntityA();
-                Category categoryEntity = categoryService.save(TestDataUtil.createTestCategoryEntityA(customerEntity));
-
-                mockMvc.perform(get("/api/categories/" + categoryEntity.getId()))
+                mockMvc.perform(get(BASE_URL + "/" + category.getId()))
                                 .andExpect(status().isOk())
-                                .andExpect(jsonPath("$.id").exists())
-                                .andExpect(jsonPath("$.name").value("Food & Beverage"))
-                                .andExpect(jsonPath("$.customer").exists())
-                                .andExpect(jsonPath("$.customer.id").exists())
-                                .andExpect(jsonPath("$.customer.username").value(customerEntity.getUsername()));
+                                .andExpect(jsonPath("$.id").value(category.getId().toString()))
+                                .andExpect(jsonPath("$.name").value("Food & Beverage"));
         }
 
         @Test
-        public void testGetCategoryReturns404WhenNotFound() throws Exception {
-                mockMvc.perform(get("/api/categories/999"))
+        @DisplayName("3. Should return 404 NOT FOUND for non-existent category")
+        public void shouldReturn404WhenCategoryNotFound() throws Exception {
+                mockMvc.perform(get(BASE_URL + "/" + UUID.randomUUID()))
                                 .andExpect(status().isNotFound());
         }
 
         @Test
-        public void testFullUpdateReturns200AndUpdatedData() throws Exception {
-                User customerEntity = TestDataUtil.createTestCustomerEntityA();
-                Category savedCategoryEntity = categoryService
-                                .save(TestDataUtil.createTestCategoryEntityA(customerEntity));
-                CategoryRequestDto updateCategory = TestDataUtil.createTestCategoryDtoA(null);
-                updateCategory.setName("UPDATED");
-                String categoryJson = objectMapper.writeValueAsString(updateCategory);
+        @DisplayName("4. Should partially update category and return 200 OK")
+        public void shouldUpdateCategoryPartially() throws Exception {
+                User user = userService.create(TestDataUtil.givenUserA());
+                Category category = categoryService.create(TestDataUtil.givenCategoryA(user));
 
-                mockMvc.perform(patch("/api/categories/" + savedCategoryEntity.getId())
+                CategoryPatchDto patchDto = CategoryPatchDto.builder()
+                                .name("Updated Name")
+                                .build();
+
+                String json = objectMapper.writeValueAsString(patchDto);
+
+                mockMvc.perform(patch(BASE_URL + "/" + category.getId())
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(categoryJson))
+                                .content(json))
                                 .andExpect(status().isOk())
-                                .andExpect(jsonPath("$.name").value("UPDATED"));
+                                .andExpect(jsonPath("$.name").value("Updated Name"));
         }
 
         @Test
-        public void testFullUpdateReturns404WhenNotExists() throws Exception {
-                UserRequestDto customerDto = TestDataUtil.createTestCustomerDtoA();
-                CategoryRequestDto categoryDto = TestDataUtil.createTestCategoryDtoA(customerDto);
-                String categoryJson = objectMapper.writeValueAsString(categoryDto);
+        @DisplayName("5. Should delete category and return 204 NO CONTENT")
+        public void shouldDeleteCategory() throws Exception {
+                User user = userService.create(TestDataUtil.givenUserA());
+                Category category = categoryService.create(TestDataUtil.givenCategoryA(user));
 
-                mockMvc.perform(put("/api/categories/999")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(categoryJson))
-                                .andExpect(status().isNotFound());
-        }
-
-        @Test
-        public void testPartialUpdateReturns200AndUpdatedField() throws Exception {
-                User customerEntity = TestDataUtil.createTestCustomerEntityA();
-                Category savedCategoryEntity = categoryService
-                                .save(TestDataUtil.createTestCategoryEntityA(customerEntity));
-                CategoryRequestDto updateCategory = TestDataUtil.createTestCategoryDtoA(null);
-                updateCategory.setName("UPDATED");
-                String categoryJson = objectMapper.writeValueAsString(updateCategory);
-
-                mockMvc.perform(patch("/api/categories/" + savedCategoryEntity.getId())
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(categoryJson))
-                                .andExpect(status().isOk())
-                                .andExpect(jsonPath("$.name").value("UPDATED"));
-        }
-
-        @Test
-        public void testDeleteCategoryReturns204() throws Exception {
-                User customerEntity = TestDataUtil.createTestCustomerEntityA();
-                Category categoryEntity = categoryService.save(TestDataUtil.createTestCategoryEntityA(customerEntity));
-
-                mockMvc.perform(delete("/api/categories/" + categoryEntity.getId()))
+                mockMvc.perform(delete(BASE_URL + "/" + category.getId()))
                                 .andExpect(status().isNoContent());
         }
 
         @Test
-        public void testDeleteCategoryReturns404WhenNotExist() throws Exception {
-                mockMvc.perform(delete("/api/categories/999"))
+        @DisplayName("6. Should return 404 when deleting non-existent category")
+        public void shouldReturn404OnDeleteIfCategoryNotFound() throws Exception {
+                mockMvc.perform(delete(BASE_URL + "/" + UUID.randomUUID()))
                                 .andExpect(status().isNotFound());
         }
+
+        @Test
+        @DisplayName("7. Should return list of all categories")
+        public void shouldReturnAllCategories() throws Exception {
+                User user = userService.create(TestDataUtil.givenUserA());
+
+                categoryService.create(TestDataUtil.givenCategoryA(user));
+                categoryService.create(TestDataUtil.givenCategoryB(user));
+                categoryService.create(TestDataUtil.givenCategoryC(user));
+
+                mockMvc.perform(get(BASE_URL + "?page=0&size=10"))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.content").isArray())
+                                .andExpect(jsonPath("$.content.length()").value(3));
+        }
+
 }
