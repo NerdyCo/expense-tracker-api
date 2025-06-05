d9eb6c3fe6884dc5ef611de1e851e06794f503d4
NerdyCo <kautsarteguh2002@gmail.com>
2025-06-05 09:49:52 +0700

    refactor transaction request mapper

--- src/main/java/com/dwi/expensetracker/mappers/impl/transaction/TransactionRequestMapperImpl.java
+++ src/main/java/com/dwi/expensetracker/mappers/impl/transaction/TransactionRequestMapperImpl.java
@@ -1,11 +1,14 @@
 package com.dwi.expensetracker.mappers.impl.transaction;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.dwi.expensetracker.domains.dtos.transaction.TransactionRequestDto;
+import com.dwi.expensetracker.domains.entities.Category;
 import com.dwi.expensetracker.domains.entities.Transaction;
+import com.dwi.expensetracker.domains.entities.User;
 import com.dwi.expensetracker.mappers.Mapper;
+import com.dwi.expensetracker.services.CategoryService;
+import com.dwi.expensetracker.services.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,15 +16,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TransactionRequestMapperImpl implements Mapper<Transaction, TransactionRequestDto> {
 
-    private final ModelMapper modelMapper;
+    private final UserService userService;
+    private final CategoryService categoryService;
 
     @Override
     public Transaction toEntity(TransactionRequestDto dto) {
-        return modelMapper.map(dto, Transaction.class);
+        // set user and category from ID
+        User user = userService.getById(dto.getUserId());
+        Category category = categoryService.getById(dto.getCategoryId());
+
+        return Transaction.builder()
+                .user(user)
+                .category(category)
+                .amount(dto.getAmount())
+                .type(dto.getType())
+                .description(dto.getDescription())
+                .date(dto.getDate())
+                .build();
     }
 
     @Override
     public TransactionRequestDto toDto(Transaction entity) {
-        return modelMapper.map(entity, TransactionRequestDto.class);
+        return TransactionRequestDto.builder()
+                .userId(entity.getUser().getId())
+                .categoryId(entity.getCategory().getId())
+                .amount(entity.getAmount())
+                .type(entity.getType())
+                .description(entity.getDescription())
+                .date(entity.getDate())
+                .build();
     }
 }
