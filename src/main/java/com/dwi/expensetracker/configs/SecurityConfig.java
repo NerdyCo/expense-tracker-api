9258a5b7826d09cffb1f7d7390d41a01ca7597e2
NerdyCo <kautsarteguh2002@gmail.com>
2025-06-27 18:15:17 +0700

    remove unrelevant files

--- src/main/java/com/dwi/expensetracker/configs/SecurityConfig.java
+++ src/main/java/com/dwi/expensetracker/configs/SecurityConfig.java
@@ -2,84 +2,12 @@ package com.dwi.expensetracker.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.dwi.expensetracker.domains.entities.User;
-import com.dwi.expensetracker.repositories.UserRepository;
-import com.dwi.expensetracker.security.AppUserDetailsService;
-import com.dwi.expensetracker.security.JwtAuthenticationFilter;
-import com.dwi.expensetracker.security.TokenBlacklistService;
-import com.dwi.expensetracker.security.authhandlers.CustomAccessDeniedHandler;
-import com.dwi.expensetracker.security.authhandlers.CustomAuthenticationEntryPoint;
-import com.dwi.expensetracker.services.AuthenticationService;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(
-            AuthenticationService authenticationService,
-            TokenBlacklistService tokenBlacklistService) {
-        return new JwtAuthenticationFilter(authenticationService, tokenBlacklistService);
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        AppUserDetailsService appUserDetailsService = new AppUserDetailsService(userRepository);
-
-        String email = "user@test.com";
-        userRepository.findByEmail(email).orElseGet(() -> {
-            User newUser = User.builder()
-                    .username("Test User")
-                    .email(email)
-                    .password(passwordEncoder().encode("password"))
-                    .build();
-
-            return userRepository.save(newUser);
-        });
-
-        return appUserDetailsService;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(
-            HttpSecurity http,
-            JwtAuthenticationFilter jwtAuthenticationFilter,
-            CustomAuthenticationEntryPoint customAuthenticationEntryPoint,
-            CustomAccessDeniedHandler customAccessDeniedHandler) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,
-                                "/api/v1/auth/login",
-                                "/api/v1/auth/refresh")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/logout").authenticated()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(customAuthenticationEntryPoint)
-                        .accessDeniedHandler(customAccessDeniedHandler))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     }
 }
--- src/main/java/com/dwi/expensetracker/security/TokenBlacklistService.java
+++
@@ -1,7 +0,0 @@
-package com.dwi.expensetracker.security;
-
-public interface TokenBlacklistService {
-    void blacklistToken(String token);
-
-    boolean isTokenBlacklisted(String token);
-}
